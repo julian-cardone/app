2a6fd7dc843448e621e32de72fc9a2150b2b2a48
Julian Cardone <juliancardone1@gmail.com>
2026-06-16 16:22:25 -0400

    remove codnitional dev bar

--- app/src/providers/AppProviders.tsx
+++ app/src/providers/AppProviders.tsx
@@ -11,13 +11,12 @@ import type { RootStackParamList } from "@/navigation/types";
 const navigationRef = createNavigationContainerRef<RootStackParamList>();
 
 export function AppProviders() {
-  const showDevBar = false;
   return (
     <GestureHandlerRootView style={styles.root}>
       <SafeAreaProvider>
         <NavigationContainer ref={navigationRef}>
           <RootNavigator />
-          {showDevBar && __DEV__ && <DevBar navigationRef={navigationRef} />}
+          {__DEV__ && <DevBar navigationRef={navigationRef} />}
         </NavigationContainer>
       </SafeAreaProvider>
     </GestureHandlerRootView>
